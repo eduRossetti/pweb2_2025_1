937b7c1330879717e73fd7011fcc21c161004229
eduRossetti <edu.rossetti100@gmail.com>
2025-03-17 17:12:07 -0300

    commit pro boina

--- app/Http/Controllers/AlunoController.php
+++ app/Http/Controllers/AlunoController.php
@@ -19,7 +19,7 @@ class AlunoController extends Controller
      */
     public function create()
     {
-        //
+        return view('aluno.form');
     }
 
     /**
---
+++ resources/views/aluno/form.blade.php
@@ -0,0 +1,7 @@
+@extends('base')
+@section('titulo', 'Listagem Aluno')
+@section('conteudo')
+
+<h3>formulario aluno</h3>
+
+@stop
