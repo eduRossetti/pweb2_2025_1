88ee61cd4015cc117fcf24fe18e9fc4c443dcfd9
eduRossetti <edu.rossetti100@gmail.com>
2025-03-28 16:25:11 -0300

    coiso pro boina

---
+++ database/factories/AlunoFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Aluno>
+ */
+class AlunoFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            "nome" => $this->faker()->name(),
+            "cpf" => $this->faker()->ramdomNumber(14, false),
+            "telefone" => $this->fake->cellphone(),
+        ];
+    }
+}
